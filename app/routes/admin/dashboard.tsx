38e7ba878a713c514a4c3ac896ba84d7be31c98d
2814109 <2814109k@gmail.com>
2022-02-06 17:10:31 +0900

    routing v2

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,6 +1,15 @@
 import { VFC } from "react";
 import { Outlet } from "remix";
 import Sidebar from "~/components/general/admin/sidebar";
+import type { LoaderFunction } from "remix";
+import { authenticator, User } from "~/utils/auth.server";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const user = await authenticator.isAuthenticated(request, {
+    failureRedirect: "/admin/login",
+  });
+  return { user };
+};
 
 const Template: VFC = () => {
   return (
